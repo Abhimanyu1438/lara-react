730565ed40b748370e07b777b5ddb4d006810d9a
Abhimanyu1438 <abhimanyuthe7888@gmail.com>
2024-07-11 23:27:26 +0530

    test

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,13 @@
+// resources/js/components/ExampleComponent.jsx
+
+import React from 'react';
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            <h1>Hello, React in Laravel!</h1>
+        </div>
+    );
+}
+
+export default ExampleComponent;
